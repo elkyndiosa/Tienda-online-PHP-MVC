99f91212bba6a6e1244bd96a612e1b1ac93dbc86
Gustavo Diosa <elkyndiosa23@gmail.com>
2020-02-01 09:57:36 -0500

    Update and rename config/db.php to  config/db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
 class database{
-    public static function connect()
-                $ db = new mysqli ( 'localhost' , 'gustavo' , 'vieja321' , 'tienda' ); 
-        $db-> query("SET NAMES 'utf8');
+    public static function connect(){
+        $db = new mysqli('160.153.129.220', 'gustavo', 'vieja321', 'tienda');
+        $db-> query("SET NAMES 'utf8'");
         return $db;
-    
+    }
 }
